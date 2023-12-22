d55eba25dbf2e0782ad99e57dc89e8191c733545
duskygloom <sarkarharsh708@gmail.com>
2023-12-22 17:28:29 +0530

    linecoding rewrite

--- src/plotter/main.py
+++ src/plotter/main.py
@@ -5,14 +5,21 @@ from plotter import Plotter
 handler = ArgHandler()
 args = handler.parse_args(sys.argv[1:])
 
-plotter = Plotter()
-plotter.setup(
-    nsignals=args.nsignal,
-    title=args.title,
-    xlabel=args.xlabel,
-    ylabel=args.ylabel,
-    xstep=args.xstep
-)
-plotter.plot(args.data)
-plotter.save(args.figfile)
-plotter.open()
+def main(args):
+    plotter = Plotter()
+    plotter.setup(
+        nsignals=args.nsignal,
+        title=args.title,
+        xlabel=args.xlabel,
+        ylabel=args.ylabel,
+        xstep=args.xstep
+    )
+    plotter.plot(args.data)
+    plotter.save(args.figfile)
+    plotter.open()
+
+if __name__ == "__main__":
+    try:
+        main(args)
+    except Exception as e:
+        print(f"[Error] Unhandled exception: {e}")
